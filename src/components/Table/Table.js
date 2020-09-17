3e21c859217c01750ce983d17d0e751e760beead
3kingz <victorkenneth95@gmail.com>
2020-09-17 20:16:29 +0100

    final touches

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,22 +4,24 @@ import "./Table.scss";
 
 function Table({ products, onDelete }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Products</th>
-          <th>Name</th>
-          <th>Quantity</th>
-          <th>Price</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {products.map((product) => (
-          <TableData product={product} key={product.id} dele={onDelete} />
-        ))}
-      </tbody>
-    </table>
+    <div style={{ overflow: "auto" }}>
+      <table>
+        <thead>
+          <tr>
+            <th>Products</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <TableData product={product} key={product.id} dele={onDelete} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
