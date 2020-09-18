f3acb1047946a78577cd23660003aa1b266ceb94
3kingz <victorkenneth95@gmail.com>
2020-09-18 16:42:08 +0100

    refactoring

--- src/components/GoBackNav/GoBackNav.js
+++ src/components/GoBackNav/GoBackNav.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./GoBackNav.scss";
 
-function GoBackNav({ nav, goBack }) {
+const GoBackNav = ({ nav, goBack }) => {
   return (
     <div className="go-back-nav" onClick={goBack}>{`Go back to ${nav}`}</div>
   );
-}
+};
 
 export default GoBackNav;
--- src/components/NoItem/NoItem.js
+++ src/components/NoItem/NoItem.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function NoItem({ container }) {
+const NoItem = ({ container }) => {
   return (
     <h2 style={{ textAlign: "center" }}>No Items In Your {container} Yet</h2>
   );
-}
+};
 
 export default NoItem;
